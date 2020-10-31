7ec669bd4dd914fa36ccf41a93f73aec14e19dfa
Oleg Konan <olegkonanff@gmail.com>
2020-10-31 16:21:04 +0300

    complete task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,23 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(string) {
+  //throw new CustomError('Not implemented');
+  const type = typeof(string);
+  let result;
+  switch (type) {
+    case "string": 
+      const curActivity = parseFloat(string, 10);
+      if (isNaN(curActivity) || curActivity <= 0 || curActivity > 15) {
+        result = false;
+      } else {
+        const k = 0.693 / HALF_LIFE_PERIOD;
+        const ln = Math.log(MODERN_ACTIVITY / curActivity);
+        result = Math.ceil(ln / k);
+      }
+      break;
+    default: 
+      result = false;
+  }
+  return result;
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const repeaterStr = String(str);
+  let result = [];
+  let additionArr = [];
+  const repeaterOptions = {
+    repeatTimes: 1,
+    separator: '+',
+    additionSeparator: '|',
+    additionRepeatTimes: 1,
+  }
+  //check if options parameters are existing and add it or change
+  if (options.repeatTimes !== undefined) {
+    repeaterOptions.repeatTimes = options.repeatTimes;
+  }
+  if (options.separator !== undefined) {
+    repeaterOptions.separator = options.separator;
+  }
+  if (options.additionSeparator !== undefined) {
+    repeaterOptions.additionSeparator = options.additionSeparator;
+  }
+  if (options.additionRepeatTimes !== undefined) {
+    repeaterOptions.additionRepeatTimes = options.additionRepeatTimes;
+  }
+  if (options.addition !== undefined) {
+    repeaterOptions.addition = String(options.addition);
+    for (let i = 0; i < repeaterOptions.additionRepeatTimes; i++) {
+      additionArr.push(repeaterOptions.addition);
+    }
+  }
+  
+  let additionStr = additionArr.join(repeaterOptions.additionSeparator)
+  
+  for (let i = 0; i < repeaterOptions.repeatTimes; i++) {
+    repeaterOptions.addition ? result.push(`${repeaterStr}${additionStr}`) : result.push(repeaterStr);
+  }
+
+  return result.join(repeaterOptions.separator);
 };
   
